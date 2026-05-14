4400f1d29b84d8030e45b1308e8044bb9da8cbaf
David <david_tjokroaminoto@tech.gov.sg>
2026-05-14 21:35:06 +0800

    Fix BatteryBar cask style

--- Casks/b/batterybar.rb
+++ Casks/b/batterybar.rb
@@ -2,11 +2,12 @@ cask "batterybar" do
   version "0.1.0"
   sha256 "87d576215edde455d0957f544eb64b419e98d70f528edd5cf127f06eda1578c5"
 
-  url "https://github.com/dchocoboo/BatteryBar/releases/download/v#{version}/BatteryBar-#{version}.zip",
-      verified: "github.com/dchocoboo/BatteryBar/"
+  url "https://github.com/dchocoboo/BatteryBar/releases/download/v#{version}/BatteryBar-#{version}.zip"
   name "BatteryBar"
   desc "Menu bar battery history monitor"
   homepage "https://github.com/dchocoboo/BatteryBar"
+
+  depends_on macos: ">= :ventura"
 
   app "BatteryBar.app"
 end
